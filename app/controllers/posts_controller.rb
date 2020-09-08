93575826ea68b3d56840658565e26a811e07d892
KRStoddard <kimberlynrstoddard@gmail.com>
2020-09-08 10:28:07 -0500

    added initial comment functionality and flash carryovers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,24 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show]
    
+    def show
+        flash[:post] = @post.id 
+    end
+    
     def new
         @post = Post.new
+        if flash[:group]
+            flash[:group]
+        end
     end
 
     def create
         @post = Post.create(post_params :title, :content) 
         if @post.valid?
             @post.update(user_id: @signed_in_user.id)
+            if flash[:group]
+                @post.update(group_id: flash[:group])
+            end
             if @post.group_id
                 redirect_to group_path(@post.group)
             else
@@ -15,8 +26,17 @@ class PostsController < ApplicationController
             end
         else
             flash[:errors] = @post.errors.full_messages
+            if flash[:group]
+                flash[:group]
+            end
             redirect_to new_post_path
         end
+    end
+
+    private
+
+    def find_post
+        @post = Post.find(params[:id])
     end
 
 end
